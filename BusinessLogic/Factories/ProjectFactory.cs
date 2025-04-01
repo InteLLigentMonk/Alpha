b54aa3e21f708791c3c97ec9325ecc8e3292cc78
Tobias Vinther <Tobias.Vinther88@gmail.com>
2025-04-01 16:00:28 +0200

    Added id to ProjectRegistrationForm in factories

--- BusinessLogic/Factories/ProjectFactory.cs
+++ BusinessLogic/Factories/ProjectFactory.cs
@@ -7,13 +7,17 @@ public class ProjectFactory
 {
     public static ProjectRegistrationForm ProjectRegistrationForm()
     {
-        return new ProjectRegistrationForm();
+        return new ProjectRegistrationForm
+        {
+            Id = new Guid()
+        };
     }
 
     public static ProjectEntity ProjectEntity(ProjectRegistrationForm project)
     {
         return new ProjectEntity
         {
+            Id = project.Id,
             ProjectName = project.ProjectName,
             ClientName = project.ClientName,
             Description = project.Description,
